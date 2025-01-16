94377b597b3f9c8c3cc6293dcec7386237845525
Aryan shukla <113294396+Aryanshukla206@users.noreply.github.com>
2025-01-16 09:06:45 -0800

    Update app.js
    Atlas Added

--- app.js
+++ app.js
@@ -20,18 +20,18 @@ const reviewRoutes = require('./routes/reviews');
 if (process.env.NODE_ENV !== "production") {
     require('dotenv').config(); 
 }
-// async function connectDB() {
-//     try {
-//         const db = await mongoose.connect(process.env.MONGO_URI);
-//         console.log("Connected to MongoDB Atlas successfully!");
-//     } catch (err) {
-//         console.error("Error connecting to MongoDB Atlas:", err.message);
-//         process.exit(1);
-//     }
-// }
-// connectDB();
-const dbURL = 'mongodb://localhost:27017/yelp-camp';
-mongoose.connect(dbURL);
+async function connectDB() {
+    try {
+        const db = await mongoose.connect(process.env.MONGO_URI);
+        console.log("Connected to MongoDB Atlas successfully!");
+    } catch (err) {
+        console.error("Error connecting to MongoDB Atlas:", err.message);
+        process.exit(1);
+    }
+}
+connectDB();
+// const dbURL = 'mongodb://localhost:27017/yelp-camp';
+mongoose.connect(db);
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", () => {
